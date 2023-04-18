b306a1ef9dcb9c89926ad509ec5da20a5c34bddc
gogostanoev <gogostanoev225@gmail.com>
2023-04-18 22:23:21 +0200

    commit one

---
+++ food_order_application/src/interfaces/order.interface.ts
@@ -0,0 +1,12 @@
+export interface Product {
+    id: string,
+    productName: string,
+    productPrice: number
+}
+
+
+export interface Order {
+    id: string,
+    orderDate: Date,
+    productsOrdered: Product[]
+}
---
+++ food_order_application/src/order/order.controller.ts
@@ -0,0 +1,16 @@
+import { Controller, Get } from '@nestjs/common';
+import { OrderService } from './order.service';
+
+@Controller('orders')
+export class OrderController {
+    constructor(
+        private readonly orderService: OrderService
+    ){}
+
+    @Get()
+    getOrders(){
+        const orders = this.orderService.getOrders();
+
+        return orders
+    }
+}
