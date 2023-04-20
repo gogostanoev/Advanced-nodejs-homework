9d4be83a615fbf5694cd2be3c3eb48b290379f0e
gogostanoev <gogostanoev225@gmail.com>
2023-04-20 22:01:27 +0200

    small fix

--- food_order_application/src/order/order.service.ts
+++ food_order_application/src/order/order.service.ts
@@ -1,35 +1,11 @@
 import { Injectable } from '@nestjs/common';
+import { ordersObj } from 'src/fakeDb/orders';
 import { Order } from 'src/interfaces/order.interface';
 
 @Injectable()
 export class OrderService {
-    orders: Order[] = [
-        {id: "1", orderDate: new Date(), productsOrdered: [{
-            id: "drinkOne",
-            productName: "Pepsi",
-            productPrice: 70
-        }]},
 
-        {id: "2", orderDate: new Date(), productsOrdered: [{
-            id: "foodOne",
-            productName: "Mimoza salad",
-            productPrice: 200
-        }]},
-
-        {id: "3", orderDate: new Date(), productsOrdered: [{
-            id: "foodTwo",
-            productName: "Chicken soup",
-            productPrice: 210
-        }]},
-        
-        {id: "4", orderDate: new Date(), productsOrdered: [{
-            id: "foodThree",
-            productName: "Tres leches cake",
-            productPrice: 180
-        }]}
-    ]
-
-    getOrders(){
-        return this.orders
+    getOrders(): Order[] {
+        return ordersObj
     }
 }
