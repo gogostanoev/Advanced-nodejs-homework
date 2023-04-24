2ea81f07befa73b0859b82e31d73f7bf3dcdf4a0
gogostanoev <gogostanoev225@gmail.com>
2023-04-24 03:57:39 +0200

    fixed

--- food_order_part2/src/order/dto/order.dto.ts
+++ food_order_part2/src/order/dto/order.dto.ts
@@ -1,20 +1,25 @@
-import { IsNotEmpty, ValidateNested } from "class-validator";
+import { Type } from "class-transformer";
+import { ArrayNotEmpty, IsNotEmpty, IsNumber, IsString, ValidateNested } from "class-validator";
 
 export class OrderDto {
     @ValidateNested({ each: true })
-    @IsNotEmpty()
+    @Type(() => ProductDto)
+    @ArrayNotEmpty()
     productsOrdered: ProductDto[]
 }
 
 
 export class ProductDto {
     @IsNotEmpty()
+    @IsString()
     id: string;
 
     @IsNotEmpty()
+    @IsString()
     productName: string;
 
     @IsNotEmpty()
+    @IsNumber()
     productPrice: number;
 }
 
