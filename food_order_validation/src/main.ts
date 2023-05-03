8d02652a0a81600c94feaf3cd85f927da3eec53d
gogostanoev <gogostanoev225@gmail.com>
2023-05-03 22:18:03 +0200

    requirements

--- food_order_validation/src/main.ts
+++ food_order_validation/src/main.ts
@@ -1,21 +1,30 @@
 // Building Food Order Application
-// Implement database in the application
+// Implement relation and authentication
 // Requirements: NOTE: Apply the requirements ON TOP OF the previous homework.
 // PART 1
 
-// Using the pgAdmin application for interacting with the local postgres database, create new databse named "food_app_db". ( At class we manually created one called "tasks" );
-// Connect/Initialize the database in the App Module ( kind reminder: see the code from class as inspiration)
-// Create new Entity class called ProductsEntity that implements the inteface Product.
-// Decorate each property of the class with @Column and @PrimaryColumn for the id property.
-// In the products module register the entity (ProductsEntity).
-// NOTE: Feel free and use the code from class and guide and inspiration.
+// Create OrderEntity that implements the Order inteface
+// The order entity should have the same properties as the interface
+// id should be primaryColumn
+// orderDate should be column
+// productsOrdered should be list of ProductEntities
+// Create the relation between the OrderEntity and ProductEntity, using the productsOrdered property. NOTE: You should create a property in the ProductEntity too, that will be named order, and it will be in relation with the OrderEntity NOTE: Think of what the relation might be, between the ProductEntity and the OrderEntity
 // PART 2
 
-// Once you have registered and implemented the database, implement the following CRUD operation in the products controller.
-// On the route Get all products, instead to return the values of the hardcoded array, return the products from the database
-// On the route Get product by id, instead of returning a product of the hardcoded array, return product by id from the database
-// Create new route called create product. when called should create and save one product entity in the database.
+// Create new module and service, users
+// In the users service hardcode few users
+// Create one method findOne that returns user by username (same as in class)
+// Create authentication on the application
+// Create new module,service called auth
+// Create local and jwt strategies
+// Create local and jwt guards
+// Create route for login that returns access_token to the user
+// Orders routes must be guarded, and to access them we should provide access_token in the requst
+// NOTE: Use the code from class as a guide and inpiration.
+
 // NOTE: Use postman to play and test the endpoint. Use the code from class as an inspiration and guide. When the homework is submitted, send the the postman collection too.
+
+// PACKAGES USED FOR AUTHENTICATION: npm install --save @nestjs/passport passport passport-local @nestjs/jwt passport-jwt npm install --save-dev @types/passport-local @types/passport-jwt
 
 
 import { NestFactory } from '@nestjs/core';
